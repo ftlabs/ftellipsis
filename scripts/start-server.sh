5017285f9096b19a0dd05d1b6850275ae6f52028
Wilson Page <wilsonpage@me.com>
2013-05-08 12:20:44 +0100

    Remove unused scripts

--- scripts/start-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# run this before https://gist.github.com/2630210
-./scripts/kill-servers.sh phantom
-./scripts/kill-servers.sh buster-server
-./node_modules/buster/bin/buster-server & # fork to a subshell
-phantomjs ./node_modules/buster/script/phantom.js &
-sleep 0.5
